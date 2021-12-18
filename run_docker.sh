5bca7d2fdc577758daafc68408e5d31193eeccc3
Nyeve <eveanyaboh@gmail.com>
2021-12-18 09:58:56 +0300

    docker image

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build -t AWSCICD:latest .
+docker build -t awscicd:latest .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker images ls
 
 # Step 3: 
 # Run flask app
-docker run -t -i -p 8080:3000 AWSCICD:latest
+docker run -t -i -p 8080:3000 awscicd :latest
