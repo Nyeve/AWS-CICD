50c6a6306de3bc63bf6ed1b1a79b0a18f4c9de3e
Nyeve <eveanyaboh@gmail.com>
2021-12-21 22:53:26 +0300

    ECR image

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,13 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 
 echo "Build Steps:"
 echo "building image..."
-docker build -t 756437418345.dkr.ecr.us-east-1.amazonaws.com .
+docker build -t 756437418345.dkr.ecr.us-east-1.amazonaws.com/capstone:latest .
 
 echo "Post-Build Steps:"
 echo "pushing image to AWS ECR..."
-docker push 756437418345.dkr.ecr.us-east-1.amazonaws.com/awscicd:latest
+docker push 756437418345.dkr.ecr.us-east-1.amazonaws.com/capstone:latest
 
 echo "updating AWS ECS service..."
-aws ecs update-service --cluster awscicd-cluster --service react-sv --force-new-deployment --no-cli-pager
+aws ecs update-service --cluster capstone-cluster --service react-sv --force-new-deployment --no-cli-pager
 
 echo "Done!"
